22d0a2c86d724fcac1b1835de51d6084722a26d0
Oliver Öhrström <xzereha@users.noreply.github.com>
2026-03-09 14:04:21 +0100

    Project Setup

---
+++ src/main/java/com/example/libraryapi/LibraryAPI.java
@@ -0,0 +1,13 @@
+package com.example.libraryapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryAPI {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LibraryAPI.class, args);
+    }
+
+}
