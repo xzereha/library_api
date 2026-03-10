a3f601daafec011a9b27a1712aa8a05542128e30
Oliver Öhrström <xzereha@users.noreply.github.com>
2026-03-10 22:19:39 +0100

    Added schema annotation for the wrapper

--- src/main/java/com/example/libraryapi/dto/ResponseWrapper.java
+++ src/main/java/com/example/libraryapi/dto/ResponseWrapper.java
@@ -4,6 +4,7 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.annotation.Nonnull;
 import jakarta.validation.constraints.Min;
 
+@Schema(description = "Wrapper for API responses, including data and version information")
 public class ResponseWrapper<T> {
     @Nonnull
     @Schema(description = "Data of the response", requiredMode = Schema.RequiredMode.REQUIRED, nullable = false)
