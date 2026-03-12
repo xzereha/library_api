7e1d23790060fb560ee7cbdad5f3063f98607d72
Oliver Öhrström <xzereha@users.noreply.github.com>
2026-03-12 11:19:44 +0100

    Changed response wrapper to record

--- src/main/java/com/example/libraryapi/dto/ResponseWrapper.java
+++ src/main/java/com/example/libraryapi/dto/ResponseWrapper.java
@@ -5,46 +5,12 @@ import jakarta.annotation.Nonnull;
 import jakarta.validation.constraints.Min;
 
 @Schema(description = "Wrapper for API responses, including data and version information")
-public class ResponseWrapper<T> {
-    @Nonnull
-    @Schema(description = "Data of the response", requiredMode = Schema.RequiredMode.REQUIRED, nullable = false)
-    private T data;
+public record ResponseWrapper<T>(
+        @Nonnull @Schema(description = "Data of the response",
+                requiredMode = Schema.RequiredMode.REQUIRED,
+                nullable = false) T data,
 
-    @Min(1)
-    @Schema(description = "API version", requiredMode = Schema.RequiredMode.REQUIRED, nullable = false)
-    private int version;
-
-    public ResponseWrapper(@Nonnull T data, @Min(1) int version) {
-        if (data == null) {
-            throw new IllegalArgumentException("Data cannot be null");
-        }
-        if (version < 1) {
-            throw new IllegalArgumentException("Version must be at least 1");
-        }
-
-        this.data = data;
-        this.version = version;
-    }
-
-    public T getData() {
-        return data;
-    }
-
-    public void setData(@Nonnull T data) {
-        if (data == null) {
-            throw new IllegalArgumentException("Data cannot be null");
-        }
-        this.data = data;
-    }
-
-    public int getVersion() {
-        return version;
-    }
-
-    public void setVersion(@Min(1) int version) {
-        if (version < 1) {
-            throw new IllegalArgumentException("Version must be at least 1");
-        }
-        this.version = version;
-    }
+        @Min(1) @Schema(description = "API version",
+                requiredMode = Schema.RequiredMode.REQUIRED,
+                nullable = false) int version) {
 }
