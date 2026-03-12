ca58110fd6f03c9ab0c500cacdce858ae67a77c3
Oliver Öhrström <xzereha@users.noreply.github.com>
2026-03-12 09:57:35 +0100

    Added formatting rules

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
 	java
 	checkstyle
+	id("com.diffplug.spotless") version "8.3.0"
 	id("org.springframework.boot") version "4.0.3"
 	id("io.spring.dependency-management") version "1.1.7"
 }
@@ -29,6 +30,19 @@ dependencies {
 	testImplementation("org.springframework.boot:spring-boot-starter-data-jpa-test")
 	testImplementation("org.springframework.boot:spring-boot-starter-webmvc-test")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+spotless {
+	java {
+		target("src/*/java/**/*.java")
+		eclipse().configFile("config/formatter/eclipse-java-style.xml")
+		trimTrailingWhitespace()
+		endWithNewline()
+	}
+}
+
+tasks.named("check") {
+	dependsOn("spotlessCheck")
 }
 
 tasks.withType<Test> {
