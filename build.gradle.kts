0b09a638413f04860227f0557f16ca036d914b11
Oliver Öhrström <xzereha@users.noreply.github.com>
2026-03-24 14:41:43 +0100

    Added rest template to the test dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -34,6 +34,7 @@ dependencies {
 	testImplementation("org.springframework.boot:spring-boot-starter-data-jpa-test")
 	testImplementation("org.springframework.boot:spring-boot-starter-webmvc-test")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("org.springframework.boot:spring-boot-starter-restclient:4.0.3")
 	testImplementation("org.junit.jupiter:junit-jupiter-api")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
